5a63df96236a7eb0f5bdcea7675fe80261eb517a
livialop <livvialn7@gmail.com>
2026-04-07 14:17:16 -0300

    correção do erro em rotas put, adição de rotas de empréstimo e implementação das regras da atividade

--- atividades/atv2/main.py
+++ atividades/atv2/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body, status
 from models import Usuario, Livro, Emprestimo
 from typing import List
+from datetime import date
 
 app = FastAPI()
 
 usuarios: List[Usuario] = []
 livros: List[Livro] = []
-emprestimo: List[Emprestimo] = []
+emprestimos: List[Emprestimo] = []
 
 '''CRUD Usuários'''
 @app.post("/usuario", response_model=Usuario, status_code=201)
@@ -25,14 +26,16 @@ def listar():
 
 
 @app.put("/usuario/{email}", response_model=Usuario)
-def atualizar(email: str, usuario: Usuario):
-    for i, u in enumerate(usuarios):
-        if u.email == email:
-            # manter email original (não alteramos identificador aqui)
-            usuarios[i] = Usuario(nome=usuario.nome, email=email, senha=usuario.senha)
-            return usuarios[i]
-    raise HTTPException(status_code=404, detail="Usuario não encontrado")
+def atualizar(email: str, senha: str, nome: str = Body(...)):
+    for usuario in usuarios:
+        if usuario.email == email:
+            # botei que pra fazer a alteracao do nome do usuario precisa validar  c senha
+            if usuario.senha != senha:
+                raise HTTPException(status_code=404, detail="senha errada")
+            usuario.nome = nome
+            return usuario
 
+    raise HTTPException(status_code=404, detail='usuario nao encontrado')
 
 @app.delete("/usuario/{email}", status_code=204)
 def deletar(email: str):
@@ -44,9 +47,16 @@ def deletar(email: str):
 
 
 '''CRUD Livros'''
+
+# Só retorna livros não emprestados
 @app.get('/livro', response_model=List[Livro])
 def listar_livros() -> List[Livro]:
-    return livros
+    livros_em_dia: List[Livro] = []
+    for livro in livros:
+        if not livro.emprestado:
+            livros_em_dia.append(livro)
+    
+    return livros_em_dia
 
 
 @app.post('/livro', response_model=Livro)
@@ -55,33 +65,74 @@ def adicionar_livro(livro: Livro) -> Livro:
 
     return livro
 
-# rotas de put estao dando conflito com o field_validator
-
 @app.delete('/livro/{isbn}')
 def deletar_livro(isbn: str) -> dict:
     for livro in range(len(livros)):
         if livros[livro].isbn == isbn:
             if not livros[livro].emprestado:
                 livros.pop(livro)
-                Livro.__values__.remove(isbn)
-                print(Livro.__values__)
+                Livro.__values__.discard(isbn) #usei o discard para não ter chance de erro usando o remove/del
+                # print(Livro.__values__)
                 return {'msg': 'livro deletado'}
             else:
                 return {'msg': 'livro está emprestado e não pode ser deletado.'}
     
     return {'msg': 'não existe livro com esse isbn'}
 
-# descricao do erro: ao atualizar a lista com o novo objeto, ele conflita com o field_validator como se o isbn fosse um novo valor repetido. 
-# talvez fazer as mudanças manualmente inves de mudar o objeto inteiro resolva, assim o isbn nao muda.
-# muda os outros campos, mantem o isbn igual.
 
-@app.put('/livro/{isbn}', response_model=List[Livro])
-def editar_livro(livro_edit: Livro) -> List[Livro]:
-    for livro in range(len(livros)):
-        if livros[livro].isbn == livro_edit.isbn:
-            livros[livro] = livro
-            return livros
+@app.put('/livro/{isbn}', response_model=Livro)
+def editar_livro(isbn: str, titulo: str = Body(...), preco: float = Body(...), autor: str = Body(...), editora: str = Body(...)) -> Livro:
+    if preco <= 0:
+        return {'msg': 'preço deve ser maior que zero.'}
     
-    return {'msg': 'não existe livro com esse isbn'}
+    for livro in livros:
+        if livro.isbn == isbn:
+            livro.titulo = titulo
+            livro.preco = preco
+            livro.autor = autor
+            livro.editora = editora
+            return livro
+
+    {'msg': 'não existe livro com esse isbn'}
+
 
 '''Empréstimo'''
+
+@app.get('/emprestimos/atrasados', response_model=List[Emprestimo])
+def listar_emprestimos_atrasados() -> List[Emprestimo]:
+    hoje = date.today()
+    emprestimos_atrasados: List[Emprestimo] = []
+    for emprestimo in emprestimos:
+        if emprestimo.prazo < hoje:
+            emprestimos_atrasados.append(emprestimo)
+    return emprestimos_atrasados
+
+
+@app.post('/emprestimo', response_model=Emprestimo)
+def criar_emprestimo(emprestimo: Emprestimo):
+    # livro existe e nao ta emprestado
+    for livro in livros:
+        if livro.isbn == emprestimo.isbn:
+            if livro.emprestado:
+                raise HTTPException(status_code=400, detail='livro ja esta em emprestimo')
+            # empresta o livro
+            livro.emprestado = True
+            emprestimos.append(emprestimo)
+            return emprestimo
+    raise HTTPException(status_code=404, detail='Livro não encontrado.')
+
+# contando como apagar um emprestimo fosse a devolucao dele
+@app.delete('/emprestimo/{isbn}')
+def apagar_emprestimo(isbn: str, email_usuario: str):
+    for emprestimo in emprestimos:
+
+        if emprestimo.isbn == isbn and emprestimo.usuario == email_usuario:
+        
+            for livro in livros:
+                if livro.isbn == isbn:
+                    livro.emprestado = False
+                    break
+        
+            emprestimos.remove(emprestimo)
+            return 
+    raise HTTPException(status_code=404, detail='emprestimo nao encontrado')
