f0028e2a3c3206aff452c2d515d2a78c9d108f46
livialop <livvialn7@gmail.com>
2026-04-07 07:40:10 -0300

    crud de livros e validator nas classes model

--- atividades/atv2/main.py
+++ atividades/atv2/main.py
@@ -1,5 +1,47 @@
+from fastapi import FastAPI
+from models import Usuario, Livro, Emprestimo
+from typing import List
+
+app = FastAPI()
+
+usuarios: List[Usuario] = []
+livros: List[Livro] = []
+emprestimo: List[Emprestimo] = []
+
 '''CRUD Usuários'''
 
 '''CRUD Livros'''
+@app.get('/livro', response_model=List[Livro])
+def listar_livros() -> List[Livro]:
+    return livros
+
+
+@app.post('/livro', response_model=Livro)
+def adicionar_livro(livro: Livro) -> Livro:
+    livros.append(livro)
+
+    return livro
+
+
+@app.delete('/livro/{isbn}')
+def deletar_livro(isbn: str):
+    for livro in range(len(livros)):
+        if livros[livro].isbn == isbn:
+            if not livros[livro].emprestado:
+                livros.pop(livro)
+            else:
+                return {'msg': 'livro está emprestado e não pode ser deletado.'}
+    
+    return {'msg': 'não existe livro com esse isbn'}
+
+
+@app.put('/livro/{isbn}', response_model=List[Livro])
+def editar_livro(isbn: str):
+    for livro in range(len(livros)):
+        if livros[livro].isbn == isbn:
+            livros[livro] = livro
+            return livros
+    
+    return {'msg': 'não existe livro com esse isbn'}
 
 '''Empréstimo'''
--- atividades/atv2/models.py
+++ atividades/atv2/models.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 class Usuario(BaseModel):
     nome: str
     email: EmailStr
     senha: str
+    __values__: set = set()
+
+    @field_validator('email')
+    @classmethod
+    def validate_unique_email(cls, value):
+        if value in cls.__values__:
+            raise ValueError("Não pode email repetido.")
+        cls.__values__.add(value)
+        return value
+
 
 class Livro(BaseModel):
     titulo: str
@@ -11,7 +21,22 @@ class Livro(BaseModel):
     preco: float
     autor: str
     editora: str
-    emprestado: bool # Se estiver emprestado, validar com True
+    emprestado: bool = Field(default=False)  # Se estiver emprestado, validar com True
+    __values__: set = set()
+
+    @field_validator('isbn')
+    @classmethod
+    def validate_unique_isbn(cls, value):
+        if value in cls.__values__:
+            raise ValueError("Não pode ISBN repetido.")
+        cls.__values__.add(value)
+        return value
+    
+    @field_validator('preco')
+    def validate_preco(cls, value):
+        if value <= 0:
+            raise ValueError("O preço deve ser maior que zero.")
+        return value
 
 class Emprestimo(BaseModel):
     isbn: str
